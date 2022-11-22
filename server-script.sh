b468830b64e3f0d0572c9bde8d6e29287e059a40
piyush-inveniai <psrivastava@inveniai.com>
2022-11-22 20:06:19 +0530

    ssh slave added

---
+++ server-script.sh
@@ -0,0 +1,15 @@
+sudo yum install java-1.8.0-open-jdk-devel -y
+sudo yum install git -y
+sudo yum install maven -y
+if [ -d "addressbook" ]
+then
+ echo 'repo is cloned and exists'
+ cd /home/ec2-user/addressbook
+ git pull origin master
+ else
+ echo "repo is not there"
+ git clone https://github.com/piyush792/addressbook.git
+ cd /home/ec2-user/addressbook
+ fi
+ mvn package
+ 
